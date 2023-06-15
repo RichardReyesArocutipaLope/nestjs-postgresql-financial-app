983435869e8b02abfefa0f70303c96ff7d95102a
Richard Arocutipa Lope <richard.arocutipa@sfact.pe>
2023-06-14 22:38:09 -0500

    Add configuration to connect postgresql with nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule { }
